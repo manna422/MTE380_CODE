614d64c3def59e91e4de0e903785bffe66ce595a
BillWenChaoJiang <jiang.bill@hotmail.com>
2017-03-14 20:41:09 -0400

    Done sensing code

--- Ultrasonic_Integration/Ultrasonic_Integration.ino
+++ Ultrasonic_Integration/Ultrasonic_Integration.ino
@@ -1,71 +1,91 @@
-// ---------------------------------------------------------------------------
-// This example code was used to successfully communicate with 15 ultrasonic sensors. You can adjust
-// the number of sensors in your project by changing SONAR_NUM and the number of NewPing objects in the
-// "sonar" array. You also need to change the pins for each sensor for the NewPing objects. Each sensor
-// is pinged at 33ms intervals. So, one cycle of all sensors takes 495ms (33 * 15 = 495ms). The results
-// are sent to the "oneSensorCycle" function which currently just displays the distance data. Your project
-// would normally process the sensor results in this function (for example, decide if a robot needs to
-// turn and call the turn function). Keep in mind this example is event-driven. Your complete sketch needs
-// to be written so there's no "delay" commands and the loop() cycles at faster than a 33ms rate. If other
-// processes take longer than 33ms, you'll need to increase PING_INTERVAL so it doesn't get behind.
-// ---------------------------------------------------------------------------
 #include <NewPing.h>
 
-#define SONAR_NUM     3 // Number of sensors.
 #define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
-#define PING_INTERVAL 33 // Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
+#define FRONT_TRIGGER_PIN 12
+#define FRONT_ECHO_PIN 11
+#define LEFT_TRIGGER_PIN 8
+#define LEFT_ECHO_PIN 7
+#define RIGHT_TRIGGER_PIN 6
+#define RIGHT_ECHO_PIN 5
+#define NUM_US_SAMPLE 3
+#define CM_WALL_TO_RAMP 34
+#define CM_POLE_TO_WALL 19
+#define DETECT_TOLERANCE 3
 
-unsigned long pingTimer[SONAR_NUM]; // Holds the times when the next ping should happen for each sensor.
-unsigned int cm[SONAR_NUM];         // Where the ping distances are stored.
-uint8_t currentSensor = 0;          // Keeps track of which sensor is active.
-
-// sonar[0] = front, sonar[1] = left, sonar[2] = right.
-// costructor format NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-NewPing sonar[SONAR_NUM] = {     // Sensor object array.
-  NewPing(12, 11, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
-  NewPing(8, 7, MAX_DISTANCE),
-  NewPing(6, 5, MAX_DISTANCE)
-};
+int leftDis_old, frontDis_old, rightDis_old;
+int leftDis, frontDis, rightDis;
+NewPing frontUS(FRONT_TRIGGER_PIN, FRONT_ECHO_PIN, MAX_DISTANCE);
+NewPing leftUS(LEFT_TRIGGER_PIN, LEFT_ECHO_PIN, MAX_DISTANCE);
+NewPing rightUS(RIGHT_TRIGGER_PIN, RIGHT_ECHO_PIN, MAX_DISTANCE);
 
 void setup() {
   Serial.begin(115200);
-  pingTimer[0] = millis() + 75;           // First ping starts at 75ms, gives time for the Arduino to chill before starting.
-  for (uint8_t i = 1; i < SONAR_NUM; i++) // Set the starting time for each sensor.
-    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+  
+  //detecting twice to fill old values set
+  getUSDis();
+  getUSDis();
 }
 
 void loop() {
-  for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through all the sensors.
-    if (millis() >= pingTimer[i]) {         // Is it this sensor's time to ping?
-      pingTimer[i] += PING_INTERVAL * SONAR_NUM;  // Set next time this sensor will be pinged.
-      if (i == 0 && currentSensor == SONAR_NUM - 1) oneSensorCycle(); // Sensor ping cycle complete, do something with the results.
-      sonar[currentSensor].timer_stop();          // Make sure previous timer is canceled before starting a new ping (insurance).
-      currentSensor = i;                          // Sensor being accessed.
-      cm[currentSensor] = 0;                      // Make distance zero in case there's no ping echo for this sensor.
-      sonar[currentSensor].ping_timer(echoCheck); // Do the ping (processing continues, interrupt will call echoCheck to look for echo).
-    }
+  getUSDis();
+  printAllUSDiff();
+  printAllUSValues();
+  
+  //assuming ramp is on left side
+  int leftDiff= leftDis - leftDis_old;
+  if(leftDiff > -1*(CM_WALL_TO_RAMP+DETECT_TOLERANCE) && leftDiff < -1*(CM_WALL_TO_RAMP-DETECT_TOLERANCE)&& leftDis!= 0 ){
+    Serial.println("ramp on left");
   }
+  
+  if(leftDiff + CM_POLE_TO_WALL < DETECT_TOLERANCE && leftDis!= 0 ){
+    Serial.println("pole on left");
+    Serial.println(leftDiff);
+    Serial.println(-1*(CM_POLE_TO_WALL-DETECT_TOLERANCE));
+  }
+  
+  if((rightDis - rightDis_old) <= (-1*(CM_POLE_TO_WALL-DETECT_TOLERANCE)) && rightDis != 0){
+    Serial.println("pole on right");
+    Serial.println((rightDis - rightDis_old));
+    Serial.println(-1*(CM_POLE_TO_WALL-DETECT_TOLERANCE));
+  }
+  
+  delay(500);
   // Other code that *DOESN'T* analyze ping results can go here.
 }
 
-void echoCheck() { // If ping received, set the sensor distance to array.
-  if (sonar[currentSensor].check_timer())
-    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
+void getUSDis(){
+  leftDis_old = leftDis;
+  rightDis_old = rightDis;
+  frontDis_old = frontDis;
+  leftDis = leftUS.convert_cm(leftUS.ping_median(NUM_US_SAMPLE));
+  rightDis = rightUS.convert_cm(rightUS.ping_median(NUM_US_SAMPLE));
+  frontDis = frontUS.convert_cm(frontUS.ping_median(NUM_US_SAMPLE));
 }
 
-void oneSensorCycle() { // Sensor ping cycle complete, do something with the results.
-  Serial.print("Left = ");
-  if(cm[1] < 10){Serial.print(" ");}
-  if(cm[1] < 100){Serial.print(" ");}  
-  Serial.print(cm[1]);
+void printUSValue(int value){
+  if(value >= 0){Serial.print(" ");}
+  if(abs(value) < 10){Serial.print(" ");}
+  if(abs(value) < 100){Serial.print(" ");}  
+  Serial.print(value);
+}
 
+void printAllUSValues(){
+  Serial.print("Ultrasonic: Left = ");
+  printUSValue(leftDis);
   Serial.print("  Front = ");
-  if(cm[0] < 10){Serial.print(" ");}
-  if(cm[0] < 100){Serial.print(" ");}  
-  Serial.print(cm[0]);
-
+  printUSValue(frontDis);
   Serial.print("  Right = ");
-  if(cm[2] < 10){Serial.print(" ");}
-  if(cm[2] < 100){Serial.print(" ");}  
-  Serial.println(cm[2]);
+  printUSValue(rightDis);
+  Serial.println();
 }
+
+void printAllUSDiff(){
+  Serial.print("Differeces: Left = ");
+  printUSValue(leftDis - leftDis_old);
+  Serial.print("  Front = ");
+  printUSValue(frontDis - frontDis_old);
+  Serial.print("  Right = ");
+  printUSValue(rightDis - rightDis_old);
+  Serial.print("   ");
+}
+
