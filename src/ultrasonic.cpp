09152f622d587cd266d51b07f802fa0cbcbecff1
andy <andyysuu@gmail.com>
2017-03-15 22:58:01 -0400

    added ultrasonic utility class

--- src/ultrasonic.cpp
+++ src/ultrasonic.cpp
@@ -1,1 +1,13 @@
 #include "ultrasonic.h"
+
+Ultrasonic::Ultrasonic(NewPings[] sensors) 
+    : mSensor(sensors) 
+{    
+}
+
+unsigned int Ultrasonic::getDistance(unsigned int* dFront, unsigned int* dLeft, unsigned int* dRight)
+{
+    *dFront = mSensors[0].convert_cm(mSensors[0].ping_median(NUM_US_SAMPLE));
+    *dLeft = mSensors[1].convert_cm(mSensors[1].ping_median(NUM_US_SAMPLE));
+    *dRight = mSensors[2].convert_cm(mSensors[2].ping_median(NUM_US_SAMPLE));
+}
--- src/ultrasonic.h
+++ src/ultrasonic.h
@@ -1,9 +1,35 @@
 #ifndef ULTRASONIC_H
 #define ULTRASONIC_H
 
+#include <NewPing.h>
+
+/**
+*   Ultrasonic utility class
+*/
 class Ultrasonic {
     public:
+        // Insert US sensors in the order of 
+        // front - left - right
+        Ultrasonic(NewPing[] sensor);
+        ~Ultrasonic();
+
+        void getDistances(unsigned int* dFront, unsigned int* dLeft, unsigned int* dRight);     
+
     private:
+        // TODO: remove unneeded consts
+        const int MAX_DISTANCE = 200;
+        const int FRONT_TRIGGER_PIN = 12;
+        const int FRONT_ECHO_PIN = 11;
+        const int LEFT_TRIGGER_PIN = 8;
+        const int LEFT_ECHO_PIN = 7;
+        const int RIGHT_TRIGGER_PIN = 6;
+        const int RIGHT_ECHO_PIN = 5;
+        const int NUM_US_SAMPLE = 3;
+        const int CM_WALL_TO_RAMP = 34;
+        const int CM_POLE_TO_WALL = 19;
+        const int DETECT_TOLERANCE = 3;
+
+        NewPing[] mSensor;
 };
 
 #endif
